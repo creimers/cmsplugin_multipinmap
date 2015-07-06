455e4bacef5c41787bd1218b4132711feb8e58ae
creimers <christoph@superservice-international.com>
2015-07-06 12:59:30 +0200

    cosmetics

--- cmsplugin_multipinmap/models.py
+++ cmsplugin_multipinmap/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
-from django.core.exceptions import ValidationError, ImproperlyConfigured
+from django.core.exceptions import ValidationError
 
 from django.conf import settings
 
