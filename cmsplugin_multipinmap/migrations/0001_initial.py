4f2a06a77e2bc34b5c07bf981e6a0f4ed88d7367
creimers <christoph@superservice-international.com>
2015-07-16 16:26:52 +0200

    migrations for djangocms 3.0

--- cmsplugin_multipinmap/migrations/0001_initial.py
+++ cmsplugin_multipinmap/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cms', '0012_auto_20150607_2207'),
+        ('cms', '0003_auto_20140926_2347'),
     ]
 
     operations = [
@@ -16,11 +16,13 @@ class Migration(migrations.Migration):
             fields=[
                 ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
                 ('name', models.CharField(max_length=50, null=True, verbose_name='name', blank=True)),
-                ('style', models.CharField(max_length=25, verbose_name='style', choices=[(b'google', b'google maps'), (b'leaflet', b'leaflet maps')])),
+                ('style', models.CharField(max_length=25, verbose_name='style', choices=[(b'google', b'Google Maps'), (b'leaflet', b'Leaflet')])),
+                ('mapbox_access_token', models.CharField(default=b'', max_length=80, blank=True, help_text='required for leaflet map style only', null=True, verbose_name='mapbox access token')),
+                ('mapbox_map_id', models.CharField(default=b'', max_length=20, blank=True, help_text='required for leaflet map style only', null=True, verbose_name='mapbox map id')),
                 ('height', models.IntegerField(default=400, help_text='height of the map in px.', verbose_name='height')),
                 ('zoom', models.IntegerField(default=8, verbose_name='zoom')),
-                ('street', models.CharField(max_length=100, verbose_name='street')),
-                ('postal_code', models.CharField(max_length=10, verbose_name='postal_code')),
+                ('street', models.CharField(help_text='address for center of map', max_length=100, verbose_name='street')),
+                ('postal_code', models.CharField(max_length=10, verbose_name='postal code')),
                 ('city', models.CharField(max_length=100, verbose_name='city')),
                 ('lat', models.DecimalField(null=True, verbose_name='lat', max_digits=10, decimal_places=6, blank=True)),
                 ('lng', models.DecimalField(null=True, verbose_name='lng', max_digits=10, decimal_places=6, blank=True)),
@@ -36,7 +38,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=50, verbose_name='name')),
                 ('street', models.CharField(max_length=100, verbose_name='street')),
-                ('postal_code', models.CharField(max_length=10, verbose_name='postal_code')),
+                ('postal_code', models.CharField(max_length=10, verbose_name='postal code')),
                 ('city', models.CharField(max_length=100, verbose_name='city')),
                 ('lat', models.DecimalField(null=True, verbose_name='lat', max_digits=10, decimal_places=6, blank=True)),
                 ('lng', models.DecimalField(null=True, verbose_name='lng', max_digits=10, decimal_places=6, blank=True)),
