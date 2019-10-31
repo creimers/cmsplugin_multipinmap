564c31784c3a6d634d57cc790dbcfe43b136ad30
Christoph Reimers <christoph@superservice-international.com>
2019-10-31 09:30:48 +0100

    use here + catch geocoding exception

--- cmsplugin_multipinmap/models.py
+++ cmsplugin_multipinmap/models.py
@@ -2,86 +2,81 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.template.loader import render_to_string
 from django.core.exceptions import ValidationError
+from django.conf import settings
 
 from cms.models import CMSPlugin
 
-from geopy.geocoders import Nominatim
+from geopy.geocoders import Here
 
 
 class Map(CMSPlugin):
 
-    STYLE_CHOICES = (
-        ('google', 'Google Maps'),
-        ('leaflet', 'Leaflet'),
-    )
+    STYLE_CHOICES = (("google", "Google Maps"), ("leaflet", "Leaflet"))
     style = models.CharField(
-        _('style'), max_length=25, choices=STYLE_CHOICES, default='leaflet')
+        _("style"), max_length=25, choices=STYLE_CHOICES, default="leaflet"
+    )
 
     leaflet_tile_url = models.CharField(
-        _('tile url'),
+        _("tile url"),
         max_length=255,
-        default='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+        default="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    )
 
     height = models.IntegerField(
-        _('height'),
-        help_text=_("height of the map in px."),
-        default=400
+        _("height"), help_text=_("height of the map in px."), default=400
     )
     ZOOM_LEVELS = map(lambda c: (c, str(c)), range(22))
-    zoom = models.IntegerField(
-        _('zoom'),
-        choices=ZOOM_LEVELS,
-        default=8
-    )
+    zoom = models.IntegerField(_("zoom"), choices=ZOOM_LEVELS, default=8)
 
     # center address
     street = models.CharField(
-        _('street'),
+        _("street"),
         max_length=100,
-        help_text=_('address for center of map'),
-        null=True,
-        blank=True
-    )
-    postal_code = models.CharField(_('postal code'), max_length=10)
-    city = models.CharField(_('city'), max_length=100)
-    lat = models.DecimalField(
-        _('lat'),
+        help_text=_("address for center of map"),
         null=True,
         blank=True,
-        decimal_places=6,
-        max_digits=10
+    )
+    postal_code = models.CharField(_("postal code"), max_length=10)
+    city = models.CharField(_("city"), max_length=100)
+    lat = models.DecimalField(
+        _("lat"), null=True, blank=True, decimal_places=6, max_digits=10
     )
     lng = models.DecimalField(
-        _('lng'),
-        null=True,
-        blank=True,
-        decimal_places=6,
-        max_digits=10
+        _("lng"), null=True, blank=True, decimal_places=6, max_digits=10
     )
 
     def clean(self, *args, **kwargs):
-        if self.style == 'mapbox' and self.mapbox_access_token == '':
-            raise ValidationError({
-                'mapbox_access_token': _('mapbox access token is required')
-                })
-        if self.style == 'mapbox' and self.mapbox_map_id == '':
-            raise ValidationError({
-                'mapbox_map_id': _('mapbox map id is required')
-                })
+        if self.style == "mapbox" and self.mapbox_access_token == "":
+            raise ValidationError(
+                {"mapbox_access_token": _("mapbox access token is required")}
+            )
+        if self.style == "mapbox" and self.mapbox_map_id == "":
+            raise ValidationError({"mapbox_map_id": _("mapbox map id is required")})
 
-        geolocator = Nominatim()
-        location = geolocator.geocode(
-            " ".join([self.street or "", self.postal_code, self.city])
-        )
-        if location:
-            self.lat = location.latitude
-            self.lng = location.longitude
-        else:
-            raise ValidationError({
-                'street': _('not a valid address'),
-                'postal_code': _('not a valid address'),
-                'city': _('not a valid address'),
-            })
+        geolocator = Here(settings.HERE_APP_ID, settings.HERE_APP_CODE)
+        try:
+            location = geolocator.geocode(
+                " ".join([self.street or "", self.postal_code, self.city])
+            )
+            if location:
+                self.lat = location.latitude
+                self.lng = location.longitude
+            else:
+                raise ValidationError(
+                    {
+                        "street": _("not a valid address"),
+                        "postal_code": _("not a valid address"),
+                        "city": _("not a valid address"),
+                    }
+                )
+        except:
+            raise ValidationError(
+                {
+                    "street": _("not a valid address"),
+                    "postal_code": _("not a valid address"),
+                    "city": _("not a valid address"),
+                }
+            )
 
     def copy_relations(self, oldinstance):
         for pin in oldinstance.pins.all():
@@ -91,69 +86,50 @@ class Map(CMSPlugin):
 
 
 class Pin(models.Model):
-    name = models.CharField(_('name'), max_length=50)
+    name = models.CharField(_("name"), max_length=50)
     map_plugin = models.ForeignKey(Map, related_name="pins")
 
-    street = models.CharField(
-        _('street'),
-        max_length=100,
-        null=True,
-        blank=True
-    )
-    postal_code = models.CharField(_('postal code'), max_length=10)
-    city = models.CharField(_('city'), max_length=100)
+    street = models.CharField(_("street"), max_length=100, null=True, blank=True)
+    postal_code = models.CharField(_("postal code"), max_length=10)
+    city = models.CharField(_("city"), max_length=100)
 
-    link = models.URLField(_('link'), blank=True, null=True)
+    link = models.URLField(_("link"), blank=True, null=True)
     link_title = models.CharField(
-        _('link title'),
-        max_length=255,
-        blank=True,
-        null=True,
+        _("link title"), max_length=255, blank=True, null=True
     )
 
-    description = models.TextField(_('description'), blank=True, null=True)
+    description = models.TextField(_("description"), blank=True, null=True)
 
     COLOR_CHOICES = (
-        ('redIcon', _('red')),
-        ('blueIcon', _('blue')),
-        ('greenIcon', _('green')),
-        ('yellowIcon', _('yellow'))
+        ("redIcon", _("red")),
+        ("blueIcon", _("blue")),
+        ("greenIcon", _("green")),
+        ("yellowIcon", _("yellow")),
     )
 
-    pin_color = models.CharField(
-        max_length=20,
-        choices=COLOR_CHOICES,
-    )
+    pin_color = models.CharField(max_length=20, choices=COLOR_CHOICES)
 
     lat = models.DecimalField(
-        _('lat'),
-        null=True,
-        blank=True,
-        decimal_places=6,
-        max_digits=10
+        _("lat"), null=True, blank=True, decimal_places=6, max_digits=10
     )
     lng = models.DecimalField(
-        _('lng'),
-        null=True,
-        blank=True,
-        decimal_places=6,
-        max_digits=10
+        _("lng"), null=True, blank=True, decimal_places=6, max_digits=10
     )
 
     @property
     def infowindow(self):
         context = {}
-        context['name'] = self.name
-        context['street'] = self.street
-        context['postal_code'] = self.postal_code
-        context['city'] = self.city
-        context['link'] = self.link
-        context['link_title'] = self.link_title
-        context['description'] = self.description
+        context["name"] = self.name
+        context["street"] = self.street
+        context["postal_code"] = self.postal_code
+        context["city"] = self.city
+        context["link"] = self.link
+        context["link_title"] = self.link_title
+        context["description"] = self.description
 
         return render_to_string(
-            'cmsplugin_multipinmap/infowindow.html', context
-        ).replace('\n', '')
+            "cmsplugin_multipinmap/infowindow.html", context
+        ).replace("\n", "")
 
     def clean(self, *args, **kwargs):
         geolocator = Nominatim()
@@ -164,11 +140,13 @@ class Pin(models.Model):
             self.lat = location.latitude
             self.lng = location.longitude
         else:
-            raise ValidationError({
-                'street': _('not a valid address'),
-                'postal_code': _('not a valid address'),
-                'city': _('not a valid address'),
-            })
+            raise ValidationError(
+                {
+                    "street": _("not a valid address"),
+                    "postal_code": _("not a valid address"),
+                    "city": _("not a valid address"),
+                }
+            )
 
     def __str__(self):
         return self.name
